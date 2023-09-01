a224f42ad39b346b8389bb178f8e12a1c606467d
Jerry <jerry.clou@outlook.com>
2023-09-01 16:53:52 +0200

    Vendredi 1 Septembre 2023

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Contact() {
+  const [author, setAuthor] = useState('');
+  const [content, setContent] = useState('');
+  const handleAuthor = (e) =>
+  {
+    setAuthor(e.target.value)
+  }
+  const handleContent = (e) => {
+    setContent(e.target.value);
+  }
+  const submit = async() =>{
+    // Envoyer la requete avec les données
+    const reponse = await fetch('/api/message',{
+      method: "POST",
+      body: JSON.stringify({author:author, content:content}),
+    });
+  }
   return (
-    <div>Contact</div>
+    <div>
+      <input placeholder='Auteur' onChange={handleAuthor}/>
+      <input placeholder='votre message' onChange={handleContent}/>
+      <button onClick={submit}>Envoyer</button>
+    </div>
   )
 }
